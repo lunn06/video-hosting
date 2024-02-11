a409691970514b1228725195d5ba7ced5ecc11c4
dcdnc <kuunii06@gmail.com>
2024-02-11 22:32:34 +0700

    run app from config

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,12 +2,15 @@ package app
 
 import (
 	"fmt"
+	"github.com/lunn06/video-hosting/internal/config"
 	"github.com/lunn06/video-hosting/internal/services"
 )
 
 func Run() {
+	cfg := config.MustLoad("configs/main.yaml")
+
 	r := services.SetupRouter()
-	err := r.Run(":8080")
+	err := r.Run(cfg.Address)
 	if err != nil {
 		fmt.Println(err)
 	}
