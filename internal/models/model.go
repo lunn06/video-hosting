f07973bc36e4bcf601e62d50248b780a87e11042
Artyom <phenix392133@mail.ru>
2024-02-22 23:59:52 +0700

    create new tables

--- internal/models/model.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type exampleModel struct {
-	Name    string
-	Example string
-}
--- internal/models/schemaModel.go
+++ internal/models/schemaModel.go
@@ -1,30 +1,45 @@
 package models
 
-// TODO: подумать над связями и какие еще таблицы могут быть
-
 var Schema = `
 CREATE TABLE IF NOT EXISTS users (
-	user_id UUID PRIMARY KEY NOT NULL,
+	id UUID PRIMARY KEY NOT NULL,
 	email TEXT UNIQUE NOT NULL,
 	channel_name TEXT NOT NULL,
 	password TEXT NOT NULL,
-	data_reg TIMESTAMP,
-	jwt TEXT NOT NULL,
-	role TEXT NOT NULL
+	data_reg TIMESTAMP NOT NULL,
+	role TEXT NOT NULL REFERENCES roles(id)
+);
+
+CREATE TABLE IF NOT EXISTS roles (
+	id UUID PRIMARY KEY,
+	role TEXT UNIQUE NOT NULL,
+	can_remove_users BOOL NOT NULL,
+	can_remove_others_videos BOOL NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS videos (
-	video_id UUID PRIMARY KEY,
-	user_id UUID REFERENCES users(user_id)	
-);
-
-CREATE TABLE IF NOT EXISTS video_attributes (
-    video_id UUID REFERENCES videos(video_id),
+    id UUID PRIMARY KEY,
 	title TEXT NOT NULL,	
-	tag TEXT,
 	localization TEXT,
 	upload_date TIMESTAMP NOT NULL ,
 	path_to_file TEXT NOT NULL,
 	count_likes INTEGER,
 	views INTEGER NOT NULL                                 
-);`
+);
+
+CREATE TABLE IF NOT EXISTS users_videos (
+	video_id UUID REFERENCES videos(id),
+	user_id UUID REFERENCES users(id)	
+);
+
+CREATE TABLE IF NOT EXISTS tags (
+	id UUID,
+	tag TEXT UNIQUE NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS videos_tags (
+	video_id UUID REFERENCES videos(id),
+	tag_id UUID REFERENCES tags(id)	
+);
+`
